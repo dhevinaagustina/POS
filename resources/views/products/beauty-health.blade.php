cfc676eb3d0fbe92898ee8bff3fd4a266995e8ea
dhevinaagustina <dhevinaagustina@gmail.com>
2025-02-22 12:07:09 +0700

    commit

--- resources/views/products/beauty-health.blade.php
+++ resources/views/products/beauty-health.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Beauty and Health/title>
+    <title>Beauty and Health</title>
 </head>
 <body>
     <h1>Daftar Produk: Beauty and Health</h1>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SalesController;
-
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,8 +28,12 @@ Route::prefix('category')->group(function () {
     Route::get('/baby-kid', [ProductController::class, 'babyKid']);
 });
 
+// User Profile
+Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
+
 // Sales Page
 Route::get('/sales', [SalesController::class, 'index']);
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
